ef98d8d2b8e7b741e4f6da290afa610166f1b2ac
Praveen Kumar <praveenkumar.m2004pk@gmail.com>
2025-12-17 10:17:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Strings Rotations of Each Other/strings-rotations-of-each-other.java
+++ Difficulty: Medium/Strings Rotations of Each Other/strings-rotations-of-each-other.java
@@ -1,60 +1,48 @@
-
 class Solution {
-    public boolean areRotations(String s1, String s2) {
-        int n = s1.length();
-        if (n != s2.length()) return false;
-        if (n == 0) return true; // both empty
-
-        // Build LPS (Longest Prefix Suffix) for the pattern s2
-        int[] lps = buildLps(s2);
-
-        // KMP search: scan conceptual text = s1 + s1 without actually concatenating
-        int i = 0; // index in pattern s2
-        int j = 0; // index in conceptual text (0..2n-1)
-
-        while (j < 2 * n) {
-            char tj = (j < n) ? s1.charAt(j) : s1.charAt(j - n); // char from s1+s1
-
-            if (tj == s2.charAt(i)) {
+    public int[] buildLCP(String s){
+        int length=s.length();
+        int i=0;
+        int j=1;
+        int[] LCP=new int[length];
+        while(i<length && j<length){
+            if(s.charAt(i)==s.charAt(j)){
+                LCP[j]=i+1;
                 i++;
                 j++;
-                if (i == n) {
-                    // matched full pattern of length n
-                    return true;
-                }
-            } else {
-                if (i != 0) {
-                    i = lps[i - 1]; // fallback within pattern using LPS
-                } else {
-                    j++; // no partial match, move forward in text
-                }
+                
+            }else{
+                i=LCP[Math.max(0,i-1)];
+                j++;
             }
         }
-
-        return false;
+        
+        
+        return LCP;
     }
-
-    // Build LPS array for KMP
-    private int[] buildLps(String pat) {
-        int m = pat.length();
-        int[] lps = new int[m];
-        int len = 0; // length of current longest prefix-suffix
-        int i = 1;
-
-        while (i < m) {
-            if (pat.charAt(i) == pat.charAt(len)) {
-                len++;
-                lps[i] = len;
+    public boolean areRotations(String s1, String s2) {
+        int[] LCP=buildLCP(s2);
+        int i=0;
+        int j=0;
+        
+        String word=s1+s1.substring(0,s1.length()-1);
+        
+        
+        while(i<s2.length() && j<word.length()){
+            if(s2.charAt(i)==word.charAt(j)){
                 i++;
-            } else {
-                if (len != 0) {
-                    len = lps[len - 1]; // try shorter prefix
-                } else {
-                    lps[i] = 0;
-                    i++;
-                }
+                j++;
+            }else if(i-1>=0){
+                i=LCP[i-1];
+            }else{
+                j++;
             }
         }
-        return lps;
+        
+        
+        
+        
+        
+        
+        return i==s2.length();
     }
 }
