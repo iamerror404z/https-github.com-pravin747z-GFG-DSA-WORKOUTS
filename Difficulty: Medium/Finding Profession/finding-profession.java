88ad750a0391c43247a1aa8c115873676f99ff2c
Praveen Kumar <praveenkumar.m2004pk@gmail.com>
2026-06-07 23:54:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Finding Profession/finding-profession.java
+++ Difficulty: Medium/Finding Profession/finding-profession.java
@@ -1,17 +1,11 @@
 class Solution {
     public int professionCode(int currLevel,int currPos,int flag,int targetLevel,int targetPos)
     {   
-        // System.out.println("curr level is :"+currLevel+"        curr pos is :"+currPos);
-        
+    
         if(currLevel==targetLevel && currPos==targetPos){
             return flag;
         }
         
-        // else if(currLevel==targetLevel){
-        //     // System.out.println(" curr pos is ddd: "+currPos);
-        // }else if(currLevel>targetLevel){
-        //     return -1;
-        // }
         
         int capacity=(int)Math.pow(2,targetLevel-currLevel)/2;
         
@@ -20,7 +14,6 @@ class Solution {
         int newPos=(currPos-1)*2;
         
         currPos+=newPos;
-        // System.out.println(capacity);
         
         
         currLevel++;
@@ -41,7 +34,7 @@ class Solution {
         String[] op=new String[]{"Engineer","Doctor"};
         int res=professionCode(1,1,0,Math.min(32,level),pos);
         
-        // System.out.println(res);
+     
   
         
         
